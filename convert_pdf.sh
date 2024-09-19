ad29e357bf95b67002ecfa63e48af5dfdabc7427
Arooran Thanabalasingam <13069024+bigaru@users.noreply.github.com>
2024-09-19 14:04:39 +0200

    add step to merge svg together

--- convert_pdf.sh
+++ convert_pdf.sh
@@ -10,5 +10,7 @@ do
     inkscape --pdf-poppler --export-type=svg -n $i -o ./$filename/page_no$i.svg $file  
 done
 
-inkscape --batch-process --actions='select-all;path-simplify;export-plain-svg'  --export-type=svg --export-overwrite ./$filename/*.svg 
+inkscape --batch-process --actions='select-all;path-simplify;export-plain-svg'  --export-type=svg --export-overwrite ./$filename/*.svg &&
+python3 svg_stack.py --direction=v --margin=50 $(ls $filename/*.svg | sort -V) > $filename.svg &&
+rm -Rf ./$filename/
 echo "completed"
